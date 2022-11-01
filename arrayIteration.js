421a99f2d3943c1c2a1993bb70bde8016428f0eb
mehjabinhasanmoni <mehjabinhasanmoni@gmail.com>
2022-11-01 21:56:13 +0600

    Array Iteratin Practice

--- arrayIteration.js
+++ arrayIteration.js
@@ -9,25 +9,18 @@ function myFunction(value,index,array){
     console.log("********");
 
 }
-
 numberForEach.forEach(myFunction);
-
 console.log(numberForEach);
 
 
 // Map
 
 const numberForMap = [45,5,10,11,50,65];
-
 function myMap(value){
-    
-    return value * 3;
-    
-
+     return value * 3;
 }
 
 const forMapValue = numberForMap.map(myMap);
-
 console.log("Previous Array is : ", numberForMap);
 console.log("New Array is : ", forMapValue);
 
@@ -35,16 +28,11 @@ console.log("New Array is : ", forMapValue);
 // Filter
 
 const numberForFilter = [45,5,10,11,50,65];
-
 function myFilter(value ,index,array){
-
     return value > 10 ;
-    
-
-}
+    }
 
 const forFilterValue = numberForFilter.filter(myFilter);
-
 console.log("Previous Array is : ", numberForFilter);
 console.log("New Array is : ", forFilterValue);
 
