1e708d1683a0026b252dae6c84caa81f32e4e439
mehjabinhasanmoni <mehjabinhasanmoni@gmail.com>
2022-11-02 00:35:32 +0600

    Problem Sove 01 November

--- problem1nov.js
+++ problem1nov.js
@@ -1,2 +1,17 @@
 // ["g","o","o","d"] show them GOOD without join() method 
 
+const senArray = ["g","o","o","d"];
+let senval = "" ;
+function mySenArray(value){
+    senval = senval + value;
+}
+senArray.forEach(mySenArray);
+console.log(senval.toUpperCase());
+
+//  You have a string "RONALDO", show the letter L in console
+
+let gString = "RONALDO";
+let gStringAns = gString.charAt(4);
+
+console.log(gStringAns);
+
