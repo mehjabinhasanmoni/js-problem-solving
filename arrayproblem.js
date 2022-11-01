0053c8059760c6d1e2af352520785be69cf8b095
mehjabinhasanmoni <mehjabinhasanmoni@gmail.com>
2022-11-01 21:02:19 +0600

    Array problem solve

--- arrayproblem.js
+++ arrayproblem.js
@@ -44,3 +44,11 @@ accNumber.sort(function(a, b){return a -b});
 
 console.log("Accsending number are :" , accNumber.join('/'));
 // console.log(accNumber.join(','));
+
+
+// Do random number
+
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(){return 0.5 - Math.random()});
+
+console.log("Random Number are :" , points);
